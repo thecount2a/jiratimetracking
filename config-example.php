d795f9db14aad5a1b6452fdc1856232155f9e6d8
Garret McGraw <gem@exhengineering.com>
2017-07-04 10:52:45 -0400

    Changed config file example

--- config-example.php
+++ config-example.php
@@ -10,3 +10,8 @@ $EXTRA_HEAD_HTML         = "";
 $OAUTH_CONSUMER_KEY      = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 $OAUTH_PRIVATE_KEY_FILE  = "/path/to/myrsakey.pem";
 $PATH_TO_HLEDGER_BINARY  = "/path/to/hledger";
+$COMPANY_PAYROLL_HEADER  = "<table cellpadding=\"3\"><tr><td rowspan=\"3\"><img width=\"80\" src=\"/path/to/logo.png\"></td><td>Example, Inc.</td></tr><tr><td>123 Anystreet Boulevard</td></tr><tr><td>Podunk, ZZ 99999</td></tr></table>";
+$STORAGE_PATH            = "/path/to/writable/storage";
+// These values must be determined by examining an export of time data from quickbooks
+$QUICKBOOKS_COMPANY_NAME = "Example, Inc";
+$QUICKBOOKS_CREATE_TIME  = "9999999999";
