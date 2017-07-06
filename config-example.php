4611179efb7cc88136870bb8f4dff0a38fb6167c
Garret McGraw <gem@exhengineering.com>
2017-07-06 18:26:53 +0000

    Set default timezone

--- config-example.php
+++ config-example.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('America/New_York');
+
 $COOKIE_PREFIX           = "company";
 $HOSTED_DOMAIN           = "www.example.com";
 $JIRA_DOMAIN             = "example.atlassian.com";
