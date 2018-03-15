ee41b43aaca9e4a9cbe8b45f01e4f52078efc73b
Garret McGraw <gem@exhengineering.com>
2018-03-15 16:46:20 -0400

    Adding php file for new task entry system

---
+++ recent.php
@@ -0,0 +1,28 @@
+<?php
+	require_once 'config.php';
+	require_once 'SupOAuthClient.php';
+	require_once 'AuthJiraCert.php';
+	require "predis/autoload.php";
+
+	$redis = new Predis\Client();
+
+	$obj = new AuthJiraCert();
+	$client = new SupOAuthClient($obj->consumerKey, $obj->privateKeyFile, $_COOKIE[$COOKIE_PREFIX."_jira_oauth_token"], $_COOKIE[$COOKIE_PREFIX."_jira_oauth_secret"]);
+	$url = $obj->jiraBaseUrl . 'rest/api/2/myself';
+	$myself = $client->performRequest($url, array("expand"=>"groups"), "GET");
+
+	if ($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		$data = json_decode(file_get_contents('php://input'), true);
+		foreach ($data as $dkey => $dvalue)
+		{
+			$redis->hSet($myself["key"].'_recentTaskMetadata', $dkey, $dvalue);
+		}
+	}
+	else
+	{
+		$data = $redis->hGetAll($myself["key"].'_recentTaskMetadata');
+		echo json_encode($data);
+	}
+	
+?>
